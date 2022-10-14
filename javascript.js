398d59411a0c1a98c540d8c4af583ee43511dd09
Monika Teiserskyte <moni6740@stud.kea.dk>
2022-10-14 10:51:27 +0200

    setting up

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
 console.log("hello");
+
+const endpoint = "#";
+
+async function getData() {
+  let result = await fetch(endpoint);
+  showPosts(await result.json);
+}
+
+function showPosts(posts) {
+  //console.log(posts);
+  posts.forEach((post) => {
+    console.log("lalala", post);
+    //template
+    const template = document.querySelector("template").content;
+    //clone
+    const clone = template.cloneNode(true);
+    //populate
+
+    //clone.querySelector("h2").textContent = movie.title.rendered;
+    //console.log(movie._embedded[wp:featuremedia])....?;
+
+    //find parent
+    const parent = document.querySelector("main");
+    parent.appendChild(clone);
+  });
+}
